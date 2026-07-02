ab0f87b2a8f071b085e606bec39ffbdda6f6ac6d
prasanna <prasanna542002@gmail.com>
2026-07-02 21:56:22 +0530

    feat(backend): connect Node.js to MySQL

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,22 @@
 const app = require("./app");
+const pool = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-});
+async function startServer() {
+  try {
+    await pool.query("SELECT 1");
+
+    console.log("✅ Connected to MySQL");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error("❌ Database connection failed");
+    console.error(err.message);
+  }
+}
+
+startServer();
--- database/init.sql
+++ database/init.sql
@@ -11,3 +11,25 @@ CREATE TABLE products (
     image_url VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(255) NOT NULL,
+    customer_email VARCHAR(255) NOT NULL,
+    total_amount DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE order_items (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+
+    FOREIGN KEY(order_id)
+        REFERENCES orders(id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY(product_id)
+        REFERENCES products(id)
+);
